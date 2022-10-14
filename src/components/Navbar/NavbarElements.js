a51cba25f81c09cc7b17b4ad87f4e8f09e4c87df
GorgeousStar <innocentblesel@gmail.com>
2022-10-14 12:30:13 +0100

    i changed the background color

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import {NavLink as Link} from 'react-router-dom';
 import {FaBars} from 'react-icons/fa';
 
 export const Nav = styled.nav`
-    background: #fff;
+    background: black;
     height: 80px;
     display: flex;
     justify-content: space-between;
